e343c8c387f54f28c5c61da222824609334a4b69
FSPA440 <79173286+FSPA440@users.noreply.github.com>
2021-05-15 14:38:52 +0200

    ajout de la methode delete

--- app/controllers/personnes_controller.rb
+++ app/controllers/personnes_controller.rb
@@ -12,7 +12,10 @@ class PersonnesController < ApplicationController
     def new
         @personne = Personne.new
     end
-    
+
+    def edit
+        @personne = Personne.find(params[:id])
+    end
 
     def create
         @personne = Personne.new(personne_params)
@@ -25,8 +28,22 @@ class PersonnesController < ApplicationController
         end
     end
 
-    private
+    def update
+        @personne = Personne.find(params[:id])
+        if @personne.update(personne_params)
+          flash[:success] = 'Personne successfully OK'
+          redirect_to personne_path(@personne)
+        end
+    end
 
+    def destroy
+        @personne = Personne.find(params[:id])
+        @personne.destroy
+        flash[:success] = 'Suppression successfully OK'
+        redirect_to personnes_path
+    end
+
+    private
     def personne_params
         params.require(:personne).permit(:prenom, :nom, :profession, :telephone, :adresse)
     end
